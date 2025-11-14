2cd2b143ea5cb78df6d568979061a95fd6b71399
wang <erwei@wangs-MacBook-Air.local>
2025-11-14 14:13:56 +0800

    update dataset

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,19 +1,19 @@
-import act
+import ast
 import torch
 from torch.utils.data import Dataset
 
 class PretrainedDataset(Dataset):
-    def __init__(self, dataset_path):
-        self.dataset_path = dataset_path
+    def __init__(self, data_path):
+        self.data_path = data_path
         self.samplers = self.load_samplers()
 
-    def load_data(self):
+    def load_samplers(self):
         samplers = []
         with open(self.dataset_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
             for line in lines:
                 line = line.strip()
-                samplers.append(act.literal_eval(line))
+                samplers.append(ast.literal_eval(line))
         return samplers
 
     def __len__(self):
@@ -24,21 +24,14 @@ class PretrainedDataset(Dataset):
         return sampler
 
     @staticmethod
-    def collate_fn(self, batch, tokenizer, max_len=512):
-        batch_size = len(batch)
-        text_tensors = []
-        mask_tensors = []
-        prey = torch.ones(batch_size, max_len)
-        for index, text in enumerate(batch):
-            txt = text["text"]
-            txt_tensor, mask_txt = tokenizer.tokenize(txt, max_len=max_len, padding=True, return_tensors="pt")
-            text_tensors.append(txt_tensor[:-1])
-            mask_tensors.append(mask_txt[1:])
-            prey[index] = (text_tensors!= tokenizer.eos_token_id)
-            prey[index] = prey[index][1:]
+    def collate_fn(batch, tokenizer):
+        input_txts = []
+        for txt in batch:
+            input_txts.append(txt["text"])
 
-        text_tensors = torch.cat(text_tensors)
-        mask_tensors = torch.cat(mask_tensors)
-        return text_tensors, prey, mask_tensors
-
-
+        token_result = tokenizer(input_txts, padding=True, truncation=True, return_tensors="pt")
+        input_ids = token_result["input_ids"]
+        X = input_ids[:, :-1]
+        Y = input_ids[:, 1:]
+        mask_loss = (X!=tokenizer.eos_token_id)
+        return X, Y, mask_loss
